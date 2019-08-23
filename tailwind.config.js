dcf196414b2f7672192e983910f64806db8da288
Rob Cameron <rob.cameron@fastmail.com>
2019-08-22 17:35:10 -0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '6rem'
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '20rem'
+      }
+    }
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+  },
+  plugins: []
+}
